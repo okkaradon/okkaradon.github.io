0cf4ebd92263dcc3d8b30af21be203c22c2572da
okkaradon <49260133+okkaradon@users.noreply.github.com>
2023-08-04 16:14:42 +0900

    笑い

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,8 @@
-let angle = 0.0009;//角回転速度 
+let angle = -0.0016566726255890928;//角回転速度 
 let xo;
 let yo;
+
+let tracearr = [];
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     canvas.style('z-index','-1');
@@ -13,11 +15,16 @@ function windowResized() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   alert = createGraphics(windowWidth, windowHeight); 
+
   
   alert.position(0,0);
   canvas.position(0,0);
   canvas.style('z-index','-1');//canvasを後ろに移動する
   alert.style('z-index','-2');
+    
+  randomslider = createSlider(0, 30, 2);
+  randomslider.position(10, 60);
+  randomslider.style('width', '200px');
 
   xo = windowWidth / 2;//xo x原点
   yo = windowHeight / 2;//yo y原点
@@ -49,15 +56,37 @@ function draw() {
 
 
     push();
-        translate(2*xo,2*yo);
+        translate(2*xo + randomslider.value() * (noise(millis())-0.5),2*yo + randomslider.value() * (noise(millis())-0.5));
 	    image(alert,-xo,-yo);
     pop();
 
+    let trace = {
+        x: mouseX-xo,
+        y: mouseY-yo
+      }
+      
+      tracearr.push(trace);
+
+    if (tracearr.length > 60) {
+        tracearr.splice(0,1);
+    }
     //線の描画
     push();
-        strokeWeight(10-movedX/4-movedY/4);
-        stroke(255);    
+        strokeWeight(1);
+        stroke(255);
+        noFill();
         translate(width/2, height/2);
+
+        beginShape();
+        for (let i = 0; i < tracearr.length; i++) {
+          let n = tracearr.length - 1 - i;
+          curveVertex(
+            tracearr[n].x * cos(angle * i) - tracearr[n].y * sin(angle * i) + randomslider.value() * (noise(millis()+pmouseX)-0.5), 
+            tracearr[n].x * sin(angle * i) + tracearr[n].y * cos(angle * i) + randomslider.value() * (noise(millis()+pmouseY)-0.5)
+          );
+        }
+        endShape();
+        /*
         line(
             //始点 マウスの座標
             mouseX-xo,
@@ -66,6 +95,7 @@ function draw() {
             (pmouseX-xo)*cos(angle)-(pmouseY-yo)*sin(angle),
             (pmouseX-xo)*sin(angle)+(pmouseY-yo)*cos(angle)
         );
+        */
     pop();
 
     print(angle);
