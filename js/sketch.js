3bb4fc30de64f72721adcf0a4ed29a94a26a5136
okkaradon <49260133+okkaradon@users.noreply.github.com>
2024-06-04 17:18:03 +0900

    ぷらいど

--- js/sketch.js
+++ js/sketch.js
@@ -16,8 +16,7 @@ let h = 0; // 色相。悲しみは虹色
 let s = 50; // 涙の彩度、元気度
 let l = 40; // 涙の明度、重み
 let t = []; // 涙のリスト。心のリスト
-let lmX, lmY; // 最後のマウス位置。涙の出発点
-let gs = 10; // グロー効果。涙が輝く
+let lmX, lmY; // 最後のマウス位置。ドットの出発点
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight); // キャンバス作るよ
@@ -31,8 +30,6 @@ function setup() {
 }
 
 function draw() {
-  // 背景を薄く再描画。涙が乾く
-  background(40, 25, 80, 0.003);
 
   for (let tear of t) {
     tear.y += tear.speed; // 涙が流れる
@@ -42,40 +39,34 @@ function draw() {
     // 涙が画面下に達したら、上からまた降る
     if (tear.y > height) {
       tear.y = -g;
-      tear.x = floor(random(width / g)) * g;
+      tear.x = floor(random(width / g)) * g; // 涙は唐突に
       tear.color = color(random(360), 40, 70); // 涙の色を変える
     }
   }
 
-  // マウスが動いたら新しい涙を作る
+  // マウスが動いたら新しいドットーを作る
   if (mouseX !== lmX || mouseY !== lmY) {
     let x = floor(mouseX / g) * g;
     let y = floor(mouseY / g) * g;
 
-    h = (h + 1) % 360; // 涙の色相を変える。虹色の悲しみ
+    h = (h + 1) % 360; // ドットドットの色相を変える。虹色の悲しみ
 
     let rs = s + random(-10, 10); // 彩度をランダムに調整。元気度の揺らぎ
-    let rl = l + random(-10, 10); // 明度をランダムに調整。涙の重みの揺らぎ
+    let rl = l + random(-10, 10); // 明度をランダムに調整。重みの揺らぎ
 
-    let c = color(h, rs, rl); // 涙の色
+    let c = color(h, rs, rl); 
 
-    // 涙にグロー効果を追加。涙が輝く
-    for (let i = gs; i > 0; i--) {
-      let a = map(i, gs, 0, 0, 0.1);
-      fill(255, 255, 255, a); // グロー効果の色と透明度
-      rect(x - i / 2, y - i / 2, g + i, g + i); // グローの描画
-    }
-
-    fill(c); // 涙の色を設定
-    rect(x, y, g, g); // 涙を描画
+    fill(c); // ドットーの色を設定
+    rect(x, y, g, g); // ドットを描画
 
     lmX = mouseX; // 最後のマウス位置を更新
     lmY = mouseY; // 最後のマウス位置を更新
   }
+  
 }
 
 function mouseMoved() {
-  // マウスが動いた時の処理。実は何もしない
+
 }
 
 function touchMoved() {
@@ -85,12 +76,18 @@ function touchMoved() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight); // キャンバスサイズをウィンドウサイズに合わせる
-  // 背景を再描画。涙の跡を消す
-  background(40, 25, 80);
+  background(40, 25, 80); // 背景を再描画。涙の跡を消す
 }
 
+function mouseClicked(){
+    if (mouseButton === LEFT) {
+        background(40, 25, 80, 0.4);
+    }
+}
+
+
 function createTears() {
-  t = []; // 涙の配列をリセット。涙を消す
+  t = []; // 配列をリセット。涙を消す
   for (let i = 0; i < 5; i++) {
     let tear = {
       x: floor(random(width / g)) * g, // 涙の初期位置（X座標）
